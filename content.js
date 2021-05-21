1c9cedca4dc30e2776c6f7dd6b722b84eb25eb7f
TriHydera <39857764+TriHydera@users.noreply.github.com>
2021-05-21 22:47:53 +0100

    new icon + code redo

--- content.js
+++ content.js
@@ -1,15 +1,21 @@
-function seesaw() {
-    document.getElementById('sp-page').style.backgroundColor = '#000'
+function ss_darkmode() {
     document.getElementsByClassName('navbar')[0].style.backgroundColor = "#000"
 
-    setTimeout(() => {
-        document.getElementsByClassName('ss-journal-header')[0].style.backgroundColor = "#000"
-        document.getElementsByClassName('ss-class-nav-content-students')[0].style.backgroundColor = "#000"
-    },4500)
+    try{
+    	var _0 = document.querySelectorAll('.ss-feed-item--avatars');
+    } catch (err){}
+	for(var i=0;i < _0.length;i++){_0[i].style.backgroundColor = "#000";}
 
-    var _0 = document.getElementsByClassName('ss-feed-item--avatars')
-    for(var i=0;i < _0.length;i++){_0[i].style.backgroundColor = "#000"}
+    document.querySelectorAll('#sp-nav')[0].style.backgroundColor = "#000";
+    document.querySelectorAll('.list-group-item')[0].style.backgroundColor = "#000";
 }
 
-seesaw()
-setInterval(seesaw(),1000)
+setTimeout(() => {
+    document.querySelectorAll('.ss-journal-header')[0].style.backgroundColor = '#000';
+    document.querySelectorAll('.sp-navbar')[0].style.backgroundColor = "#000";
+    document.getElementById('sp-page').style.backgroundColor = '#000'
+document.getElementById('class-list')[0].style.backgroundColor = "#000"
+    
+    ss_darkmode();
+    },4500);
+setInterval(ss_darkmode(),2000);
