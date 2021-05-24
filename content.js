dec3f5052c673473985e92e5497aa4a027eb336d
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2021-05-24 21:43:23 +0100

    new setTimeout() function

--- content.js
+++ content.js
@@ -1,21 +1,21 @@
 function ss_darkmode() {
     document.getElementsByClassName('navbar')[0].style.backgroundColor = "#000"
 
-    try{
-    	var _0 = document.querySelectorAll('.ss-feed-item--avatars');
-    } catch (err){}
-	for(var i=0;i < _0.length;i++){_0[i].style.backgroundColor = "#000";}
-
-    document.querySelectorAll('#sp-nav')[0].style.backgroundColor = "#000";
-    document.querySelectorAll('.list-group-item')[0].style.backgroundColor = "#000";
-}
+    
+    setTimeout(() => {
+		if(location.href.split('/#/')[1].includes('class')){
+		    var _0 = document.querySelectorAll('.ss-feed-item--avatars');
+		    for(var i=0;i < _0.length;i++){_0[i].style.backgroundColor = "#000";}}
+			document.querySelectorAll('.ss-journal-header')[0].style.backgroundColor = '#000';
+		    document.getElementById('class-list').style.backgroundColor = "#000"
+		    document.querySelectorAll('.list-group-item')[0].style.backgroundColor = "#000";
+			document.querySelectorAll('#sp-nav')[0].style.backgroundColor = "#000";
+		},1000)
 
 setTimeout(() => {
-    document.querySelectorAll('.ss-journal-header')[0].style.backgroundColor = '#000';
-    document.querySelectorAll('.sp-navbar')[0].style.backgroundColor = "#000";
-    document.getElementById('sp-page').style.backgroundColor = '#000'
-document.getElementById('class-list')[0].style.backgroundColor = "#000"
-    
-    ss_darkmode();
-    },4500);
+	document.querySelectorAll('.sp-navbar')[0].style.backgroundColor = "#000";
+	document.getElementById('sp-page').style.backgroundColor = '#000';
+
+	ss_darkmode()
+},4500);
 setInterval(ss_darkmode(),2000);
